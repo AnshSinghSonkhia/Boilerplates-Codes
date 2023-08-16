7228025d198abf726e38fea31b62767dfc0f74bc
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-08-16 19:19:29 +0530

    Create Java Boilerplate.java

---
+++ Java Boilerplate.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram {
+
+   /* Change
+    *  "MyFirstJavaProgram"
+    * to your file name
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
